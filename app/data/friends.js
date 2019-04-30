198813140c13bce07eaf60f6f981aaf7ddb0b8d2
Keiran Hamilton <keiranhamilton@gmail.com>
2019-04-30 12:48:46 -0400

    tweaks

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,49 +3,49 @@ var friends = [
     name: "Nicholas Cage",
     photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYqDXqMObIJzTk9fzZB7PauZTQzgXU9vd6PT4bEzMhorkl05_T",
-    scores: ["1", "5", "5", "1", "5", "5", "1", "1", "1", "5"]
+    scores: ["3", "5", "5", "4", "5", "5", "4", "5", "3", "5"]
   },
   {
     name: "Christian Bale",
     photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlmc-MqJEobwalfBC9o-AGEC6sjKNe7BQGvyvpWQ8MBnmoK95F",
-    scores: ["2", "4", "1", "1", "1", "1", "1", "2", "1", "5"]
+    scores: ["2", "4", "3", "3", "5", "3", "3", "2", "4", "5"]
   },
   {
     name: "Anthony Mackie",
     photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnCID-osPCt8HyLn6KcqFXI7yPiK-3HnY3m7IqoQcybb4LvWj0HQ",
-    scores: ["4", "3", "5", "1", "1", "5", "5", "1", "1", "1"]
+    scores: ["4", "3", "5", "3", "3", "5", "5", "2", "3", "5"]
   },
   {
     name: "Denzel Washington",
     photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGZqUBpaNK6DwWozeDDsWgaNmc3V-0A2spfgrCBnvSZ4TIYbq2tA",
-    scores: ["5", "1", "2", "1", "3", "4", "1", "3", "3", "4"]
+    scores: ["5", "5", "4", "1", "3", "4", "4", "3", "3", "4"]
   },
   {
     name: "The Wolverine",
     photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQufRYp6JXcePeU8ohobiyg0RXrZ0c75qWVKdxxiw3Gs1N6dzNw",
-    scores: ["1", "1", "5", "5", "5", "5", "5", "5", "5", "1"]
+    scores: ["4", "3", "5", "5", "4", "4", "4", "5", "5", "2"]
   },
   {
     name: "Beyonce",
     photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRogFlniX3T3mKwkkr3MbII7hwandXRw-mtqAM4YssH0iKE-q7wzQ",
-    scores: ["1", "5", "5", "1", "5", "5", "1", "1", "1", "5"]
+    scores: ["3", "5", "5", "4", "5", "5", "3", "4", "3", "5"]
   },
   {
     name: "Lauryn Hill",
     photo:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpmuor-vWU9VpZFXnpLw7xI6G6T46jcYfC7kaAkYNVn4WhCOEV5A",
-    scores: ["2", "4", "1", "1", "1", "1", "1", "2", "1", "5"]
+    scores: ["2", "4", "3", "5", "4", "3", "4", "2", "3", "5"]
   },
   {
     name: "Jennifer Lawrence",
     photo:
       "http://1.bp.blogspot.com/-bSZHjwmXm2M/UQidt7QaIoI/AAAAAAAAEsQ/XaA08Lsx6so/s1600/HQ-Jennifer-at-the-Silver-Linings-Playbook-photocall-at-TIFF-07-09-12-jennifer-lawrence-32118401-3000-2189.jpg",
-    scores: ["4", "3", "5", "1", "1", "5", "5", "1", "1", "1"]
+    scores: ["4", "3", "5", "2", "3", "5", "5", "4", "4", "3"]
   },
   {
     name: "Kerry Washington",
